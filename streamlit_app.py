37a79b87ca635eb41a089a659a847e7a1808c4ad
Matthew McAuley <mmcaw@Matthews-MacBook-Air-2.local>
2023-06-01 11:18:48 +0100

    renaming

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,39 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+query_dropdown = "SELECT DISTINCT(UUID) FROM `qc-database-365211.Chip_QA.QA_Spectra_Table` LIMIT 10"
+
+spectra_options = pd.read_gbq(query_dropdown, credentials=credentials)
+spectra_options = spectra_options["UUID"].tolist()
+
+option = st.selectbox(
+    'Select a UUID',
+    spectra_options)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+query = f"SELECT * FROM `qc-database-365211.Chip_QA.QA_Spectra_Table` WHERE UUID='{option}'"
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+df = pd.read_gbq(query, credentials=credentials)
 
-    points_per_turn = total_points / num_turns
+st.line_chart(df, x="Wavelength", y="Counts")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
