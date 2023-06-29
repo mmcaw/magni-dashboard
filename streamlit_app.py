f3007297a8d3e4e5926073658c0c5af7b9f2a292
Matthew McAuley <mmcaw@Matthews-MacBook-Air-2.local>
2023-06-29 15:20:37 +0100

    mvp

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
+import plotly.express as px
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
@@ -11,7 +12,7 @@ client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 def run_query(query):
     query_job = client.query(query)
     rows_raw = query_job.result()
@@ -20,20 +21,67 @@ def run_query(query):
     return rows
 
 
-query_dropdown = "SELECT DISTINCT(UUID) FROM `qc-database-365211.Chip_QA.QA_Spectra_Table` LIMIT 10"
+col1, col2, _ = st.columns(3)
 
-spectra_options = pd.read_gbq(query_dropdown, credentials=credentials)
-spectra_options = spectra_options["UUID"].tolist()
+query_dropdown = "SELECT DISTINCT(System) FROM `qc-database-365211.System_References.abc`"
+system_options = pd.read_gbq(query_dropdown, credentials=credentials)
+system_options = system_options["System"].tolist()
+with col1: 
+    system = st.selectbox(
+        'Select a System',
+        system_options)
 
-option = st.selectbox(
-    'Select a UUID',
-    spectra_options)
+with col2:
+    channel = st.selectbox(
+        'Select a Channel',
+        [1, 2])
 
+# query_dropdown = "SELECT DISTINCT(Date) FROM `qc-database-365211.System_References.abc`"
+# spectra_options = pd.read_gbq(query_dropdown, credentials=credentials)
+# spectra_options = spectra_options["Date"].tolist()
+# date = st.selectbox(
+#     'Select a Date',
+#     spectra_options)
 
-query = f"SELECT * FROM `qc-database-365211.Chip_QA.QA_Spectra_Table` WHERE UUID='{option}'"
+col1, col2, _ = st.columns(3)
 
+with col1:
+    date_start = st.date_input(
+            "Date From")
+
+with col2:
+    date_end = st.date_input(
+            "Date To")
+
+query = f"SELECT * FROM `qc-database-365211.System_References.abc` WHERE Date between '{date_start}' and '{date_end}' AND Channel={channel} AND System='{system}'"
 df = pd.read_gbq(query, credentials=credentials)
 
-st.line_chart(df, x="Wavelength", y="Counts")
+df["Wavelengths"] = df["Spectra"].apply(lambda x: x[0]["Wavelengths"])
+df["Counts"] = df["Spectra"].apply(lambda x: x[0]["Counts"])
+
+df = df.explode(["Wavelengths", "Counts"])
+
+
+
+## DOWNLOADING
+@st.cache_data()
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    df_export = df.copy()
+    df_export = df_export.pivot_table(["Counts"],["Wavelengths"], ["System","Date","Channel"])
+    return df_export.to_csv().encode('utf-8')
+
+st.download_button(
+    label="Download data as CSV",
+    data=convert_df(df),
+    file_name='reference_spectra.csv',
+    mime='text/csv',
+)
+
+
+fig = px.line(df, x="Wavelengths", y="Counts", color='Date')
+st.plotly_chart(fig, theme="streamlit")
+
+# st.line_chart(df, x="Wavelengths", y="Counts")
 
 
